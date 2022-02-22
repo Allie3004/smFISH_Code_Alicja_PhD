8660651aebc0fddf6770dccc0ae9b3ba2598318f
Alicja Brozek <ab6715@ic.ac.uk>
2022-02-22 19:40:34 +0000

    Update cell_renamer.py

--- cell_renamer.py
+++ cell_renamer.py
@@ -5,14 +5,14 @@ import os
 os.chdir("/Users/alicja/Desktop/egl-18_cy5_elt-1_red_in_ceh_mut")
 file = pd.read_csv(r'egl-18_at_26hrs_ceh_mut.csv', header = 0)
 hours_of_development = 26
-worms = file['Worm'].values.ravel()
-worms = pd.unique(worms)
+#worms = file['Worm'].values.ravel()
+# worms = pd.unique(worms)
 sets = 1
 l2_cells = {'V1':['h','j','k','l'],'V2':['n','m','t','y'],'V3':['u','i','o','p'],'V4':['1','2','3','5'],'V5':['6','7','8','9'],'V6':['0','e','v','b'],'T':['+','.','<','-']}
 #print(l2_cells['V1'][1])
-cells = 'V1','V2','V3','V4','V5','V6','T'
+cells = 'V1','V2','V3','V4','V6','T'
 #cells = 'V1','V2','V3'
-#worms = 1,6,87
+worms = 1,6,87
 sets = 'a'
 for worm in worms:
 
@@ -26,6 +26,25 @@ for worm in worms:
         set['ROI_attribute'] = set['ROI_attribute'].replace({'f':'H2a','g':'H2p'})
     elif 'g' in set['ROI_attribute'].unique():
         set['ROI_attribute'] = set['ROI_attribute'].replace({'g':'H2'})
+    if l2_cells['V5'][3] in set['ROI_attribute'].unique():
+        if l2_cells['V5'][1] in set['ROI_attribute'].unique():
+            if l2_cells['V5'][0] in set['ROI_attribute'].unique():
+                if l2_cells['V5'][2] in set['ROI_attribute'].unique():
+                    set['ROI_attribute'] = set['ROI_attribute'].replace({l2_cells['V5'][0]:'V5aa',l2_cells['V5'][1]:'V5ap',l2_cells['V5'][2]:'V5pa',l2_cells['V5'][3]:'V5pp'})
+                else:
+                    set['ROI_attribute'] = set['ROI_attribute'].replace({l2_cells['V5'][0]:'V5aa',l2_cells['V5'][1]:'V5ap',l2_cells['V5'][3]:'V5p'})
+            elif l2_cells['V5'][2] in set['ROI_attribute'].unique():
+                set['ROI_attribute'] = set['ROI_attribute'].replace({l2_cells['V5'][1]:'V5a',l2_cells['V5'][2]:'V5pa',l2_cells['V5'][3]:'V5pp'})
+            else:
+                set['ROI_attribute'] = set['ROI_attribute'].replace({l2_cells['V5'][1]:'V5a',l2_cells['V5'][3]:'V5p'})
+        elif l2_cells['V4'][3] in set['ROI_attribute'].unique():
+            if l2_cells['V4'][1] in set['ROI_attribute'].unique():
+                set['ROI_attribute'] = set['ROI_attribute'].replace({l2_cells['V5'][3]:'V5pp'})
+        elif l2_cells['V6'][3] in set['ROI_attribute'].unique():
+            if l2_cells['V6'][1] in set['ROI_attribute'].unique():
+                set['ROI_attribute'] = set['ROI_attribute'].replace({l2_cells['V5'][3]:'V5pp'})
+        else:
+            set['ROI_attribute'] = set['ROI_attribute'].replace({l2_cells['V5'][3]:'V5'})
 
     for cell in cells:
         if (l2_cells[str(cell)][0] in set['ROI_attribute'].unique() and l2_cells[str(cell)][2]) in set['ROI_attribute'].unique():
@@ -36,9 +55,6 @@ for worm in worms:
             set['ROI_attribute'] = set['ROI_attribute'].replace({l2_cells[str(cell)][1]:str(cell)+'a',l2_cells[str(cell)][2]:str(cell)+'pa',l2_cells[str(cell)][3]:str(cell)+'pp'})
         elif l2_cells[str(cell)][1] in set['ROI_attribute'].unique() and l2_cells[str(cell)][3] in set['ROI_attribute'].unique():
             set['ROI_attribute'] = set['ROI_attribute'].replace({l2_cells[str(cell)][1]:str(cell)+'a',l2_cells[str(cell)][3]:str(cell)+'p'})
-        if cell == 'V5':
-            if 'e' in set['ROI_attribute'].unique() and l2_cells['V5'][3] in set['ROI_attribute'].unique() :
-                set['ROI_attribute'] = set['ROI_attribute'].replace({'V5pp'})
         elif l2_cells[str(cell)][3] in set['ROI_attribute'].unique():
             set['ROI_attribute'] = set['ROI_attribute'].replace({l2_cells[str(cell)][3]:str(cell)})
     """
@@ -47,4 +63,5 @@ for worm in worms:
     else:
         print('b')
     """
-    print(set)
+    file.update(set)
+print(file)
